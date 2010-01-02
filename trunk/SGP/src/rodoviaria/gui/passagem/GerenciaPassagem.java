7926ae27e1c98e2fb130983765be99ae2eaf811b
jader_james@hotmail.com <jader_james@hotmail.com@5414d1d6-ee70-11de-83d8-6d5adf01d787>
2010-01-02 16:36:15 +0000

    iniciando passagem

--- trunk/SGP/src/rodoviaria/gui/passagem/GerenciaPassagem.java
+++ trunk/SGP/src/rodoviaria/gui/passagem/GerenciaPassagem.java
@@ -4,6 +4,7 @@ package rodoviaria.gui.passagem;
  *
  * @author Jader
  */
+import administracao.database.DataBaseManagerImpl;
 import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -16,49 +17,41 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
 
 public class GerenciaPassagem extends JFrame {
+
+    private TableModel tableModel;
+    private JTable resultTable;
 
     public GerenciaPassagem() {
 
         setTitle("Gerencia Passagens");
-        setSize(800, 600);
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         JTabbedPane abas = new JTabbedPane();
+
         ImageIcon passagemIcon = createImageIcon("edit_f2.png");
         ImageIcon viagemIcon = createImageIcon("viagem.png");
 
-        JPanel panel1 = new JPanel();//("Panel #1");
-        JPanel panel2 = new JPanel();//("Panel #2");
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
 
+        abas.add(panel1,"1 Panel");
+        abas.add(panel2,"2 Panel");
 
-        //tabbedPane.addTab("Tab 1", viagemIcon, panel1,
-                //"Does nothing");
-        //tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
-        //tabbedPane.addTab("Tab 2", passagemIcon, panel2,
-                //"Does twice as much nothing");
-        //tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-
-
-        //panel4.setPreferredSize(new Dimension(410, 50));
 
         //Add the tabbed pane to this panel.
-        //add(tabbedPane);
+        add(abas);
 
         //The following line enables to use scrolling tabs.
         //tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
 
-    protected JComponent makeTextPanel(String text) {
-        JPanel panel = new JPanel(false);
-        JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(filler);
-        return panel;
-    }
-
+   
     /** Returns an ImageIcon, or null if the path was invalid. */
     protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = GerenciaPassagem.class.getResource(path);
@@ -70,36 +63,12 @@ public class GerenciaPassagem extends JFrame {
         }
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from
-     * the event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("TabbedPaneDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Add content to the window.
-        frame.add(new GerenciaPassagem(), BorderLayout.CENTER);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String[] args)
+    {
+        GerenciaPassagem gp = new GerenciaPassagem();
+        gp.setVisible(true);
     }
-
-    public static void main(String[] args) {
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        SwingUtilities.invokeLater(new Runnable() {
-
-            public void run() {
-                //Turn off metal's use of bold fonts
-                UIManager.put("swing.boldMetal", Boolean.FALSE);
-                createAndShowGUI();
-            }
-        });
-    }
+        
 }
 
 

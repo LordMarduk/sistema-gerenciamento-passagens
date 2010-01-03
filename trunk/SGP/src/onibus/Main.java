1d180fb207f01cd7ea6bedb839339e4f9b153012
roneioliveira@gmail.com <roneioliveira@gmail.com@5414d1d6-ee70-11de-83d8-6d5adf01d787>
2010-01-03 02:36:02 +0000

    Mandando:
    API de Codigo de Barras, e o bcWebCam portable by Ronei

--- trunk/SGP/src/onibus/Main.java
+++ trunk/SGP/src/onibus/Main.java
@@ -1,10 +1,26 @@
-/*
- * Classe Principal do nucleo do ONIBUS
- * 
+/*Main - Executa o programa bcWebCam, ler o codigo de barras.
+ * Passa o codigo lido para validaPassagem.
+ * Se ValidaPassagem retorna true, a passagem é aceita caso contario invalida
  */
 
 package onibus;
 
+import java.io.IOException;
+
 public class Main {
 
+    public static void main (String [] a){
+
+       try
+       {
+          Runtime.getRuntime().exec("bcWebCam\\bcWebCam - Portable.exe");
+       }
+       catch(IOException iOException)
+       {
+          iOException.printStackTrace();
+       }
+
+        ValidaPassagem vp = new ValidaPassagem();
+        
+    }
 }

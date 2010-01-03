c79f44cac01e6b2394169b6570673a2c3351883f
roneioliveira <roneioliveira@gmail.com>
2010-01-03 02:36:02 +0000

    Mandando:
    API de Codigo de Barras, e o bcWebCam portable by Ronei

---
+++ SGP/src/onibus/ValidaPassagem.java
@@ -0,0 +1,18 @@
+/* ValidaPassagem - Recebe o codigo da passagem e verifica no bd
+ * em passagem->passagem_pk se o codigo bate e se o mesmo tem a coluna do BD
+ * passagemValida=true, ou seja, if(numPassagem == passagem_pk && passagemValida==true)
+ * se sim retorna true para main, caso contrario false.
+ * Obs.: Se a pessagem for válida, antes de retorna true para main, ele seta
+ * passagemValida como false, para evitar que ela seja usada outra vez.
+ */
+
+package onibus;
+
+class ValidaPassagem {
+
+    public ValidaPassagem(){
+
+
+    }
+
+}
